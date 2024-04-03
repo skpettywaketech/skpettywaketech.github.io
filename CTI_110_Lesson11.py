a82c8f80c51b9a6b125a29cc47d9487cbc284882
skpettywaketech <160439516+skpettywaketech@users.noreply.github.com>
2024-04-03 16:58:49 -0400

    new

---
+++ CTI_110_Lesson11.py
@@ -0,0 +1,20 @@
+# day = 25
+# month = 'December'
+# temp = -15
+# day_name = 'Wednesday'
+
+# print(f"Today is {day_name}, {month} {day} and it's {temp} degrees outside")
+
+light_is_on = True
+
+if light_is_on: 
+    print("The light is on!")
+else:
+    print("We're in the dark")
+
+age = 17
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Child")
